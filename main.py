a9f5cf5d6945fbd1e9d3e24348307817c3783f99
Jenrola Odunolaoluwa <jenrola2292@gmail.com>
2022-09-26 18:42:16 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import torch
+from torch.quantization import quantize_dynamic
+from transformers import pipeline, AutoModelForSeq2SeqLM, AutoTokenizer
+model_ckpt = "google/pegasus-cnn_dailymail"
+
+
+class Model:
+    def __init__(self, checkpoint = model_ckpt, quantize = True):
+        model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+        if quantize:
+            model = quantize_dynamic(model, {torch.nn.Linear}, dtype = torch.qint8)
+        tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+        self.pipe = pipeline('summarization', model = model, tokenizer = tokenizer)
+
+    def predict(self, text):
+        return self.pipe(text)
+
+app = FastAPI()
+model = Model()
+
+class request_body(BaseModel):
+    article: str
+
+@app.post('/generate')
+async def generate_summary(data : request_body):
+    summary = model.predict(data.article)
+    return {"summary": summary}
